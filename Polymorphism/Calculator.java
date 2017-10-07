2489b732846ca5cfefd54ea637b773822632c684
Terobero <kaanbozkurt2001@gmail.com>
2017-10-07 14:53:50 +0300

    Update Calculator.java

--- Polymorphism/Calculator.java
+++ Polymorphism/Calculator.java
@@ -1,22 +1,22 @@
 //Polymorphism, Override & Inheritance Example
-public class Calculator extends Computer{
+public class Calculator extends Computer{ //Inheritance example
 	String name;
 	String batteryType;
 	
 	public Calculator(String name, String batteryType){
-		super(name);
+		super(name); //More inheritance
 		this.batteryType = batteryType;
 	}
 	
 	public String toString(){
-		return super.toString() + " and has a "  + batteryType + " battery";
+		return super.toString() + " and has a "  + batteryType + " battery"; //Returning toString() method of Computer + the battery type
 	}
 	
 	public int addition(int a, int b){
-		return super.addition(a, b);
+		return super.addition(a, b); //Returning addition(int a, int b) method of Computer
 	}
 	
 	public int addition(int a, int b, int c){
-		return super.addition(a, b, c);
+		return super.addition(a, b, c); //Returning addition(int a, int b, int c) method of Computer
 	}
 }
