ec861b45361d4fc5e693b9ef572bb3a13000e4fa
Terobero <kaanbozkurt2001@gmail.com>
2017-10-07 14:42:17 +0300

    Update Main.java

--- Polymorphism/Main.java
+++ Polymorphism/Main.java
@@ -1,8 +1,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Computer comp = new Computer("comp");
-		Computer calc = new Calculator("calc", "AAA");
+		Computer comp = new Computer("comp"); //Creating a computer object
+		Computer calc = new Calculator("calc", "AAA"); //Creating a calculator object
 		System.out.println(comp.toString() + ".\n" + calc.toString() + ".");
 		System.out.println("Computer can do addition: 1 + 2 = " + comp.addition(1, 2) + " / 1 + 2 + 3 = " + comp.addition(1, 2, 3));
 		System.out.println("Calculator can also do addition: 1 + 2 = " + calc.addition(1, 2) + " / 1 + 2 + 3 = " + calc.addition(1, 2, 3));
